c82d498bc3b3bbd66e79490300922326686b4612
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-03-03 11:33:22 +0330

    production.rb edited

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :stage, :production
 # Replace 127.0.0.1 with your server's IP address!
-server '192.69.208.220', user: 'deploy', roles: %w{web app}
+server '192.69.208.220', user: 'deploy', roles: %w{web app db}
